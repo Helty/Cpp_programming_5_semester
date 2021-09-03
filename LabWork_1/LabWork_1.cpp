678193c9b6002d8f2e566b14cac0ea5d31b225a6
Andrey <globalshow6@gmail.com>
2021-09-03 21:11:11 +0300

    Первая лабораторная работа

--- LabWork_1/LabWork_1.cpp
+++ LabWork_1/LabWork_1.cpp
@@ -1,20 +1,62 @@
-﻿// LabWork_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <conio.h>
 
-#include <iostream>
+using namespace std;
+
+void OutputInformationAboutMe();
+int GetNumberOperand();
+int OutputAndReturnSum(int);
+
+void OutputInformationAboutMe()
+{
+    cout << "Шестаков БИ-31" << endl << endl;
+}
+
+int GetNumberOperand()
+{
+    int NumberOperands;
+    do
+    {
+        cout << "Введите какое колличество операндов вы хотите использовать (максимально 4, минимум 1): ";
+        cin >> NumberOperands;
+    } while ((NumberOperands > 4) or (NumberOperands < 1));
+    return NumberOperands;
+}
+
+int OutputAndReturnSum(int NumberOperands)
+{
+    int Counter = 0;
+    int Sum = 0;
+    for (int i = 0; i < NumberOperands; i++)
+    {
+        Counter = 0;
+        cout << "Введите " << i + 1 << " операнд: ";
+        cin >> Counter;
+        Sum += Counter;
+    }
+    cout << endl << "Сумма ваших операндов: " << Sum << endl << endl;
+
+    return Sum;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Rus");
+
+    OutputInformationAboutMe();
+
+    do
+    {
+        int NumberOperands = GetNumberOperand();
+
+        int Sum = OutputAndReturnSum(NumberOperands);
+
+        cout << "MOD: " << Sum % 10 << endl << endl;
+        cout << "Для выхода из программы нажмите Esc, для продолжения Enter." << endl << endl;
+        cout << "-----------------------------------------------------------------------------------------------------" << endl;
+
+    } while (_getch() != 27);
+
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
