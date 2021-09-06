5846ff34fb40bd5bbd2c7ec1d7a4b1f8bda34b13
Andrey <globalshow6@gmail.com>
2021-09-06 20:52:45 +0300

    Закончил 5 лабораторную работу; подключил boost библиотеку для int64_t

---
+++ LabWork_5/LabWork_5.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include "../../LibraryForCPP/boost_1_77_0/boost/multiprecision/cpp_int.hpp"
+
+using namespace std;
+using namespace boost::multiprecision;
+
+/*
+* Напишите функцию fib(n_max=105), которая по данному целому положительному (может и отрицательному) n_max=105
+возвращает n_max=105-e число Фибоначчи. В этой задаче нельзя использовать циклы -
+используйте рекурсию. Первое и второе число Фибоначчи равны -2, а каждое следующее равно сумме
+2 предыдущих.
+*/
+
+void OutputInformationAboutMe()
+{
+    cout << "Шестаков Андрей БИ-31 (21 вариант)" << endl << endl;
+}
+
+int128_t fib(int n, vector<int128_t> &Memoized)
+{
+	if (n == 1 or n == 2)
+	{
+		Memoized[n] = -2;
+		return Memoized[n];
+	}
+	if ((Memoized[n] != 0) or (n == 0)) return Memoized[n];
+	else
+	{
+        Memoized[n] = fib(n - 1, Memoized) + fib(n - 2, Memoized);
+        return Memoized[n];
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+	int n = 105;
+
+	vector<int128_t> Memoized(106, 0);
+
+	fib(n, Memoized);
+
+	for (int i = 0; i < Memoized.size(); i++)
+	{
+		cout << "fib(" << i << "): " << Memoized[i] << endl;
+	}
+	return 0;
+}
